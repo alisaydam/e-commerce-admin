a90124c863096f029e734adfdc18c7851fd2f0e6
Ali Saydam <alsydm091@gmail.com>
2023-07-04 09:16:42 +0300

    Clerk Added with api keys and stuff

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const SetupPage = () => {
+  return (
+    <p>
+      <Button>This is a protected Route</Button>
+    </p>
+  );
+};
+
+export default SetupPage;
