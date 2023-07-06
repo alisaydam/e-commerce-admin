95376555846853f4f1b1fc15bbb2b6d4178339dd
Ali Saydam <alsydm091@gmail.com>
2023-07-06 05:04:39 +0300

    5:40

--- app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -7,15 +7,15 @@ import { Billboard } from "@prisma/client";
 import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import React from "react";
-import { BillboardColumn, columns } from "./columns";
+import { CategoryColumn, columns } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
 import { ApiList } from "@/components/ui/api-list";
 
-interface BillboardClientProps {
-  data: BillboardColumn[];
+interface CategoryClientProps {
+  data: CategoryColumn[];
 }
 
-export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
+export const CategoryClient: React.FC<CategoryClientProps> = ({ data }) => {
   const router = useRouter();
   const params = useParams();
 
@@ -23,21 +23,21 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title={`Billboards (${data.length})`}
-          description="Manage billboardsfor your store"
+          title={`Categories (${data.length})`}
+          description="Manage categories for your store"
         />
         <Button
-          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+          onClick={() => router.push(`/${params.storeId}/categories/new`)}
         >
           <Plus className="mr-2 h-4 2-4" />
           Add New
         </Button>
       </div>
       <Separator />
-      <DataTable columns={columns} data={data} searchKey="label" />
-      <Heading title="API" description="API calls for Billboards" />
+      <DataTable columns={columns} data={data} searchKey="name" />
+      <Heading title="API" description="API calls for Categories" />
       <Separator />
-      <ApiList entitiyName="billboards" entityIdName="billboardId" />
+      <ApiList entitiyName="catgories" entityIdName="categoryId" />
     </>
   );
 };
---
+++ components/ui/api-list.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useOrigin } from "@/hooks/use-origin";
+import { useParams } from "next/navigation";
+import { ApiAlert } from "@/components/ui/api-alert";
+
+interface ApiListProps {
+  entitiyName: string;
+  entityIdName: string;
+}
+
+export const ApiList: React.FC<ApiListProps> = ({
+  entitiyName,
+  entityIdName,
+}) => {
+  const params = useParams();
+  const origin = useOrigin();
+
+  const baseUrl = `${origin}/api/${params.storeId}`;
+  return (
+    <>
+      <ApiAlert
+        title="GET"
+        variant="public"
+        description={`${baseUrl}/${entitiyName}`}
+      />
+      <ApiAlert
+        title="GET"
+        variant="public"
+        description={`${baseUrl}/${entitiyName}/{${entityIdName}}`}
+      />
+      <ApiAlert
+        title="POST"
+        variant="admin"
+        description={`${baseUrl}/${entitiyName}`}
+      />
+      <ApiAlert
+        title="PATCH"
+        variant="admin"
+        description={`${baseUrl}/${entitiyName}/{${entityIdName}}`}
+      />
+      <ApiAlert
+        title="DETELE"
+        variant="admin"
+        description={`${baseUrl}/${entitiyName}/{${entityIdName}}`}
+      />
+    </>
+  );
+};
